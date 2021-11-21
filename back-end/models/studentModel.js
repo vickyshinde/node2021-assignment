336d696ca05789f4553975ff6ab5e186dbad7b80
Vicky Shinde <vicky.shinde@weboniselab.com>
2021-11-21 12:27:48 +0530

    join table

--- back-end/models/studentModel.js
+++ back-end/models/studentModel.js
@@ -1,13 +1,17 @@
-const db = require("../services/db");
+const db = require('../services/db');
 
 class StudentModel {
   static getAll() {
-    let sql = "SELECT * from Student";
+    // let sql = 'SELECT * from Student';
+    let sql = `SELECT Student.id, Student.role, Student.id, Student.fname, Student.lname, Student.email, Student.password, Student.city, Student.contact, DepartmentStudent.DeptId AS DeptId, Department.name AS Deptname FROM Student JOIN DepartmentStudent
+  ON (Student.id=DepartmentStudent.StudId) JOIN Department ON (Department.id=DepartmentStudent.DeptId);`;
     return db.execute(sql);
   }
 
   static getById(id) {
-    let sql = `SELECT * FROM Student WHERE id = ${id};`;
+    // let sql = `SELECT * FROM Student WHERE id = ${id};`;
+    let sql = `SELECT Student.id, Student.role, DepartmentStudent.StudId AS StudId, Student.fname, Student.lname, Student.email, Student.password, Student.city, Student.contact, DepartmentStudent.DeptId AS DeptId, Department.name AS Deptname FROM Student JOIN DepartmentStudent
+  ON (Student.id=DepartmentStudent.StudId) JOIN Department ON (Department.id=DepartmentStudent.DeptId) WHERE StudId = ${id};`;
     return db.execute(sql);
   }
 
@@ -26,8 +30,9 @@ class StudentModel {
 
   static postNewStudent(student) {
     console.log(student);
-    const { role, fname, lname, email, password, city, contact } = student;
-    let sql = `INSERT INTO Student (role, fname, lname, email, password, city, contact) value (${role}, '${fname}', '${lname}', '${email}', '${password}', '${city}', ${contact})`;
+    const { role, fname, lname, email, password, city, contact, userType, DeptId, Deptname } = student;
+    // let sql = `INSERT INTO Student (role, fname, lname, email, password, city, contact, userType) value (${role}, '${fname}', '${lname}', '${email}', '${password}', '${city}', ${contact}, 0); SET @Student_id = LAST_INSERT_ID(); INSERT INTO DepartmentStudent (StudId, DeptId) VALUES(@Student_id, ${DeptId});`;
+    let sql = `INSERT INTO Student (role, fname, lname, email, password, city, contact, userType) value (${role}, '${fname}', '${lname}', '${email}', '${password}', '${city}', ${contact}, 0); SET @Student_id = LAST_INSERT_ID(); INSERT INTO DepartmentStudent (StudId, DeptId) VALUES(@Student_id, ${DeptId});`;
     return db.execute(sql);
   }
 }
